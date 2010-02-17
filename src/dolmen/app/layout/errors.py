429263b5d3c7de219f011f6672ddbe42e65e2231
Souheil CHELFOUH <trollfot@vittra.org>
2010-02-17 17:44:36 +0100

    Locating the Error, to be able to use the layout transparently.

--- src/dolmen/app/layout/errors.py
+++ src/dolmen/app/layout/errors.py
@@ -3,6 +3,8 @@
 import grok
 from dolmen.app.layout import Page
 from zope.publisher.interfaces import INotFound
+from zope.location import LocationProxy
+from zope.site.hooks import getSite
 
 grok.templatedir("templates")
 
@@ -11,14 +13,18 @@ class NotFound(Page):
     grok.name("index.html")
     grok.context(INotFound)
     grok.require("zope.Public")
+
+    @apply
+    def context():
+        """This is done to avoid redefining context in the __init__, after
+        calling Page.__init__. This way, the located error is the one and
+        only context.
+        """
+        def fset(self, error):
+            self._context = LocationProxy(error, error.ob, "Not found")
+        def fget(self):
+            return self._context
+        return property(fget, fset)
         
     def update(self):
         self.request.response.setStatus(404)
-
-    def application_url(self, name=None):
-        obj = self.context.ob
-        while obj is not None:
-            if isinstance(obj, grok.Application):
-                return self.url(obj, name)
-            obj = obj.__parent__
-        return self.request.URL.get(0)
