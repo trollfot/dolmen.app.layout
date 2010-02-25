37d54eb33395ee041df9b305c0e05a745b6917bf
Souheil CHELFOUH <trollfot@vittra.org>
2010-02-25 20:31:16 +0100

    setu version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join
 
 name = 'dolmen.app.layout'
-version = '0.5dev'
+version = '0.5'
 readme = open(join('src', 'dolmen', 'app', 'layout', 'README.txt')).read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
